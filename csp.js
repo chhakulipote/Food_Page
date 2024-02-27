db42c64ee630d29621c41a075675e1514e1052bf
chhakulipote <140889954+chhakulipote@users.noreply.github.com>
2024-02-27 14:16:25 +0530

    Add files via upload

---
+++ csp.js
@@ -0,0 +1,17 @@
+
+// function search() {
+//     var searchTerm = document.getElementById('searchInput').value.toLowerCase();
+//     var dishes = document.getElementById('dishes').getElementsByTagName('option');
+//     var found = false;
+//     for (var i = 0; i < dishes.length; i++) {
+//         if (dishes[i].value.toLowerCase() === searchTerm) {
+//             found = true;
+//             document.getElementById('searchResult').innerHTML = "Found: " + dishes[i].value;
+//             break;
+//         }
+//     }
+//     if (!found) {
+//         document.getElementById('searchResult').innerHTML = "No matching dish found.";
+//     }
+//     return false; // Prevent form submission
+// }
